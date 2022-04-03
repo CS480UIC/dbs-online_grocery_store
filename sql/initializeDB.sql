14418fd8896670dd0e0cf60bc00a6894c101fadd
KenHwa <97709402+KenHwa@users.noreply.github.com>
2022-04-02 22:06:14 -0500

    Update initializeDB.sql

--- sql/initializeDB.sql
+++ sql/initializeDB.sql
@@ -1,299 +1,98 @@
--- MySQL dump 10.13  Distrib 8.0.28, for Win64 (x86_64)
---
--- Host: localhost    Database: online_grocery_store
--- ------------------------------------------------------
--- Server version	8.0.28
+CREATE TABLE customer (
+	username VARCHAR(20) UNIQUE NOT NULL,
+	password VARCHAR(20 NOT NULL,
+	first_name VARCHAR(20) NOT NULL,
+	last_name VARCHAR(20) NOT NULL,
+	email VARCHAR(50) UNIQUE NOT NULL,
+	address VARCHAR(50) NOT NULL,
+	PRIMARY KEY (username)
+);
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
---
--- Table structure for table `credit_card`
---
+CREATE TABLE credit_card (
+	credit_card_number bigint NOT NULL,
+	cvv VARCHAR(3) NOT NULL, 
+	expiration_date VARCHAR(20) NOT NULL,
+	credit_card_name VARCHAR(30) NOT NULL,
+	PRIMARY KEY (credit_card_number)
+);
 
-DROP TABLE IF EXISTS `credit_card`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `credit_card` (
-  `credit_card_number` bigint NOT NULL,
-  `cvv` varchar(3) NOT NULL,
-  `expiration_date` varchar(20) NOT NULL,
-  `credit_card_name` varchar(30) NOT NULL,
-  PRIMARY KEY (`credit_card_number`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+CREATE TABLE history(
+	order_id VARCHAR(20),
+	items INT NOT NULL,
+	order_date DATE NOT NULL,
+	username VARCHAR(20),
+	PRIMARY KEY(order_id),
+	FOREIGN KEY(username) REFERENCES customer(username)
+);
 
---
--- Dumping data for table `credit_card`
---
+CREATE TABLE delivery_info(
+	driver_id INT,
+	username VARCHAR(20),
+	driver_proximity TIME NOT NULL,
+	vehicle_type VARCHAR(20) NOT NULL,
+	PRIMARY KEY(driver_id),
+	FOREIGN KEY(username) REFERENCES customer(username)
+);
 
-LOCK TABLES `credit_card` WRITE;
-/*!40000 ALTER TABLE `credit_card` DISABLE KEYS */;
-INSERT INTO `credit_card` VALUES (2222420000001113,'123','08/22','John'),(4007702835532454,'332','01/24','Amy'),(4263982640269299,'590','04/24','Jill'),(4917484589897107,'34','06/28','Ken'),(5425233430109903,'112','09/25','Tim');
-/*!40000 ALTER TABLE `credit_card` ENABLE KEYS */;
-UNLOCK TABLES;
+CREATE TABLE shopping_cart(
+	product_id INT,
+	product_price DECIMAL(5,2) UNSIGNED NOT NULL,
+	quantity INT NOT NULL,
+	username VARCHAR(20),
+	PRIMARY KEY(product_id),
+	FOREIGN KEY(username) REFERENCES customer(username)
+);
 
---
--- Table structure for table `customer`
---
 
-DROP TABLE IF EXISTS `customer`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `customer` (
-  `username` varchar(20) NOT NULL,
-  `password` varchar(20) NOT NULL,
-  `first_name` varchar(20) NOT NULL,
-  `last_name` varchar(20) NOT NULL,
-  `email` varchar(50) NOT NULL,
-  `address` varchar(50) NOT NULL,
-  PRIMARY KEY (`username`),
-  UNIQUE KEY `username` (`username`),
-  UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+CREATE TABLE fruit(
+	product_id INT,,
+	fruit_name VARCHAR(20),
+	fruit_price DECIMAL(4,2) NOT NULL,
+	fruit_quantity int NOT NULL,
+	fruit_picture VARCHAR(20) NOT NULL,
+	PRIMARY KEY (product_id),
+	FOREIGN KEY (product_id) references shopping_cart (product_id)
+);
+CREATE TABLE vegetable(
+	product_id INT,
+	veg_name VARCHAR(20),
+	veg_price DECIMAL(4,2)NOT NULL,
+	veg_quantity INT NOT NULL,
+	veg_picture VARCHAR(20)NOT NULL,
+	PRIMARY KEY(product_id),
+	FOREIGN KEY(product_id) REFERENCES shopping_cart(product_id)
+);
 
---
--- Dumping data for table `customer`
---
+CREATE TABLE meat(
+	product_id INT,
+	meat_name VARCHAR(20),
+	Meat_price DECIMAL(5,2) UNSIGNED NOT NULL,
+	meat_weight INT NOT NULL,
+	meat_picture VARCHAR(20)NOT NULL,
+	PRIMARY KEY(product_id),
+	FOREIGN KEY(product_id) REFERENCES shopping_cart(product_id)
+);
 
-LOCK TABLES `customer` WRITE;
-/*!40000 ALTER TABLE `customer` DISABLE KEYS */;
-INSERT INTO `customer` VALUES ('hellodill','ArthurDW*','Dill','Pickle','dpill@yahoo.com','892 Fairfield Street\nDefiance, OH 43512'),('itsalighani98','coolguy89','Ali','Ghani','itsalighani98@gmail.com','244 Gladiolus Drive, Romeovile, IL'),('roflgator','noshot78','Bill','Burr','billerstin@gmai.com','92 Grant Drive\nGrandville, MI 49418'),('sickdude0','hioASchas','Jill','Stein','jstein@hotmail.com','121 Streetsvile Lane, Bolingbrook, IL, 60528'),('unicorncandy','elohelad09x','Jeff','Bezos','jeffyB@amazon.com','7135 Pendergast Road\nLa Vergne, TN 37086');
-/*!40000 ALTER TABLE `customer` ENABLE KEYS */;
-UNLOCK TABLES;
 
---
--- Table structure for table `delivery_info`
---
 
-DROP TABLE IF EXISTS `delivery_info`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `delivery_info` (
-  `driver_id` int NOT NULL,
-  `username` varchar(20) DEFAULT NULL,
-  `driver_proximity` varchar(20) NOT NULL,
-  `vehicle_type` varchar(20) NOT NULL,
-  PRIMARY KEY (`driver_id`),
-  KEY `username` (`username`),
-  CONSTRAINT `delivery_info_ibfk_1` FOREIGN KEY (`username`) REFERENCES `customer` (`username`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `delivery_info`
---
 
-LOCK TABLES `delivery_info` WRITE;
-/*!40000 ALTER TABLE `delivery_info` DISABLE KEYS */;
-INSERT INTO `delivery_info` VALUES (9088,'hellodill','20 minutes','truck'),(12354,'roflgator','80 minutes','truck'),(78787,'sickdude0','4 hours','plane'),(90909,'unicorncandy','1 hour 30 minutes','truck'),(99087,'itsalighani98','3 hours','truck');
-/*!40000 ALTER TABLE `delivery_info` ENABLE KEYS */;
-UNLOCK TABLES;
 
---
--- Table structure for table `entity1`
---
 
-DROP TABLE IF EXISTS `entity1`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `entity1` (
-  `username` varchar(50) NOT NULL,
-  `password` varchar(50) NOT NULL,
-  `email` varchar(50) NOT NULL,
-  PRIMARY KEY (`username`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `entity1`
---
+CREATE TABLE user
+(
+  username VARCHAR(50) primary key,
+  `password` VARCHAR(50) NOT NULL,
+  email VARCHAR(50) NOT NULL
+);
 
-LOCK TABLES `entity1` WRITE;
-/*!40000 ALTER TABLE `entity1` DISABLE KEYS */;
-/*!40000 ALTER TABLE `entity1` ENABLE KEYS */;
-UNLOCK TABLES;
 
---
--- Table structure for table `fruit`
---
+CREATE TABLE entity1 
+(
+  username VARCHAR(50) primary key,
+  `password` VARCHAR(50) NOT NULL,
+  email VARCHAR(50) NOT NULL
+);
 
-DROP TABLE IF EXISTS `fruit`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `fruit` (
-  `product_id` int NOT NULL,
-  `fruit_name` varchar(20) DEFAULT NULL,
-  `fruit_price` decimal(4,2) NOT NULL,
-  `fruit_quantity` int NOT NULL,
-  `fruit_picture` varchar(20) NOT NULL,
-  PRIMARY KEY (`product_id`),
-  CONSTRAINT `fruit_ibfk_1` FOREIGN KEY (`product_id`) REFERENCES `shopping_cart` (`product_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `fruit`
---
-
-LOCK TABLES `fruit` WRITE;
-/*!40000 ALTER TABLE `fruit` DISABLE KEYS */;
-INSERT INTO `fruit` VALUES (7875,'Orange',7.00,5,''),(8775,'Banana',5.00,4,''),(9876,'Apple',4.00,3,''),(898898,'Watermelon',3.00,1,''),(988889,'Kiwi',6.00,2,'');
-/*!40000 ALTER TABLE `fruit` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `history`
---
-
-DROP TABLE IF EXISTS `history`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `history` (
-  `order_id` varchar(20) NOT NULL,
-  `items` int NOT NULL,
-  `order_date` date NOT NULL,
-  `username` varchar(20) DEFAULT NULL,
-  PRIMARY KEY (`order_id`),
-  KEY `username` (`username`),
-  CONSTRAINT `history_ibfk_1` FOREIGN KEY (`username`) REFERENCES `customer` (`username`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `history`
---
-
-LOCK TABLES `history` WRITE;
-/*!40000 ALTER TABLE `history` DISABLE KEYS */;
-INSERT INTO `history` VALUES (' 234',2,'2021-10-14','itsalighani98'),(' 235',2,'2021-10-10','sickdude0'),(' 236',5,'2021-10-14','unicorncandy'),('237',1,'2021-10-16','hellodill'),('238',8,'2021-10-02','roflgator');
-/*!40000 ALTER TABLE `history` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `meat`
---
-
-DROP TABLE IF EXISTS `meat`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `meat` (
-  `product_id` int NOT NULL,
-  `meat_name` varchar(20) DEFAULT NULL,
-  `meat_price` float NOT NULL,
-  `meat_weight` int NOT NULL,
-  `meat_picture` varchar(20) NOT NULL,
-  PRIMARY KEY (`product_id`),
-  CONSTRAINT `meat_ibfk_1` FOREIGN KEY (`product_id`) REFERENCES `shopping_cart` (`product_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `meat`
---
-
-LOCK TABLES `meat` WRITE;
-/*!40000 ALTER TABLE `meat` DISABLE KEYS */;
-INSERT INTO `meat` VALUES (7875,'beef',1,1,'hj'),(8775,'chicken',2,1,'hj'),(9876,'goat',1,1,'hj'),(898898,'turkey',1,1,'i'),(988889,'fish',1,1,'j');
-/*!40000 ALTER TABLE `meat` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `shopping_cart`
---
-
-DROP TABLE IF EXISTS `shopping_cart`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `shopping_cart` (
-  `product_id` int NOT NULL,
-  `product_price` decimal(5,2) unsigned NOT NULL,
-  `quantity` int NOT NULL,
-  `username` varchar(20) DEFAULT NULL,
-  PRIMARY KEY (`product_id`),
-  KEY `username` (`username`),
-  CONSTRAINT `shopping_cart_ibfk_1` FOREIGN KEY (`username`) REFERENCES `customer` (`username`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `shopping_cart`
---
-
-LOCK TABLES `shopping_cart` WRITE;
-/*!40000 ALTER TABLE `shopping_cart` DISABLE KEYS */;
-INSERT INTO `shopping_cart` VALUES (7875,5.99,5,'unicorncandy'),(8775,2.99,10,'sickdude0'),(9876,12.99,1,'itsalighani98'),(898898,10.99,8,'roflgator'),(988889,6.78,30,'hellodill');
-/*!40000 ALTER TABLE `shopping_cart` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `user`
---
-
-DROP TABLE IF EXISTS `user`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `user` (
-  `username` varchar(50) NOT NULL,
-  `password` varchar(50) NOT NULL,
-  `email` varchar(50) NOT NULL,
-  PRIMARY KEY (`username`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `user`
---
-
-LOCK TABLES `user` WRITE;
-/*!40000 ALTER TABLE `user` DISABLE KEYS */;
-/*!40000 ALTER TABLE `user` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `vegetable`
---
-
-DROP TABLE IF EXISTS `vegetable`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `vegetable` (
-  `product_id` int NOT NULL,
-  `veg_name` varchar(20) DEFAULT NULL,
-  `veg_price` decimal(4,2) NOT NULL,
-  `veg_quantity` int NOT NULL,
-  `veg_picture` varchar(20) NOT NULL,
-  PRIMARY KEY (`product_id`),
-  CONSTRAINT `vegetable_ibfk_1` FOREIGN KEY (`product_id`) REFERENCES `shopping_cart` (`product_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `vegetable`
---
-
-LOCK TABLES `vegetable` WRITE;
-/*!40000 ALTER TABLE `vegetable` DISABLE KEYS */;
-INSERT INTO `vegetable` VALUES (7875,'Artichoke',3.99,1,'yi'),(8775,'Asparagus',12.99,2,'hi'),(9876,'Brocolli',2.99,3,'hello'),(898898,'Carrot',1.99,4,'lol'),(988889,'Spinach',2.99,1,'yu');
-/*!40000 ALTER TABLE `vegetable` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2022-04-02 21:40:16
