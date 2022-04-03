2a16e8c3f484fde252912a9a76d4cc4cd5a951a4
KenHwa <97709402+KenHwa@users.noreply.github.com>
2022-04-02 19:10:16 -0500

    Update queries.sql

--- sql/queries.sql
+++ sql/queries.sql
@@ -1,1 +1,27 @@
+SELECT * from customer
+where username = 'itsalighani98'
+order by username asc;
 
+SELECT * from fruit
+where product_id = 1
+order by product_id asc;
+
+SELECT * from meat
+where product_id = 124
+order by product_id asc;
+
+SELECT * from vegetable
+where product_name = 'Brocolli'
+order by product_name asc;
+
+SELECT product_name from fruit
+where count(product_name) = 1
+order by product_name asc;
+
+SELECT order_id from history
+where DATE(order_date) = '	10/14/2021'
+order by order_id asc;
+
+SELECT product_name from fruit
+order by product_name asc
+having COUNT(fruit_quantity) = 1;
