81ad9bebabf7a12660478fc53504fe4fa21ab5f7
Ali Ghani <52897968+alighani98@users.noreply.github.com>
2022-05-05 20:40:28 -0500

    Update initializeDB.sql

--- sql/initializeDB.sql
+++ sql/initializeDB.sql
@@ -1,98 +1,79 @@
-CREATE TABLE customer (
-	username VARCHAR(20) UNIQUE NOT NULL,
-	password VARCHAR(20 NOT NULL,
-	first_name VARCHAR(20) NOT NULL,
-	last_name VARCHAR(20) NOT NULL,
-	email VARCHAR(50) UNIQUE NOT NULL,
-	address VARCHAR(50) NOT NULL,
-	PRIMARY KEY (username)
+CREATE TABLE `customer` (
+  `username` varchar(20) NOT NULL,
+  `password` varchar(20) NOT NULL,
+  `first_name` varchar(20) NOT NULL,
+  `last_name` varchar(20) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `address` varchar(50) NOT NULL,
+  `credit_card_number` bigint DEFAULT NULL,
+  PRIMARY KEY (`username`),
+  UNIQUE KEY `username` (`username`),
+  UNIQUE KEY `email` (`email`),
+  UNIQUE KEY `credit_card_number` (`credit_card_number`)
 );
 
 
-CREATE TABLE credit_card (
-	credit_card_number bigint NOT NULL,
-	cvv VARCHAR(3) NOT NULL, 
-	expiration_date VARCHAR(20) NOT NULL,
-	credit_card_name VARCHAR(30) NOT NULL,
-	PRIMARY KEY (credit_card_number)
+CREATE TABLE `credit_card` (
+  `credit_card_number` bigint NOT NULL,
+  `cvv` varchar(3) NOT NULL,
+  `expiration_date` varchar(20) NOT NULL,
+  `credit_card_name` varchar(30) NOT NULL,
+  PRIMARY KEY (`credit_card_number`),
+  CONSTRAINT `credit_card_ibfk_1` FOREIGN KEY (`credit_card_number`) REFERENCES `customer` (`credit_card_number`)
 );
 
-CREATE TABLE history(
-	order_id VARCHAR(20),
-	items INT NOT NULL,
-	order_date DATE NOT NULL,
-	username VARCHAR(20),
-	PRIMARY KEY(order_id),
-	FOREIGN KEY(username) REFERENCES customer(username)
+CREATE TABLE `delivery_info` (
+  `driver_id` int NOT NULL,
+  `username` varchar(20) DEFAULT NULL,
+  `driver_proximity` varchar(20) NOT NULL,
+  `vehicle_type` varchar(20) NOT NULL,
+  PRIMARY KEY (`driver_id`),
+  KEY `username` (`username`),
+  CONSTRAINT `delivery_info_ibfk_1` FOREIGN KEY (`username`) REFERENCES `customer` (`username`)
 );
 
-CREATE TABLE delivery_info(
-	driver_id INT,
-	username VARCHAR(20),
-	driver_proximity TIME NOT NULL,
-	vehicle_type VARCHAR(20) NOT NULL,
-	PRIMARY KEY(driver_id),
-	FOREIGN KEY(username) REFERENCES customer(username)
+CREATE TABLE `fruit` (
+  `product_id` int NOT NULL,
+  `fruit_name` varchar(20) DEFAULT NULL,
+  `fruit_price` decimal(4,2) NOT NULL,
+  `fruit_quantity` int NOT NULL,
+  `fruit_picture` varchar(20) NOT NULL,
+  PRIMARY KEY (`product_id`)
 );
 
-CREATE TABLE shopping_cart(
-	product_id INT,
-	product_price DECIMAL(5,2) UNSIGNED NOT NULL,
-	quantity INT NOT NULL,
-	username VARCHAR(20),
-	PRIMARY KEY(product_id),
-	FOREIGN KEY(username) REFERENCES customer(username)
+CREATE TABLE `history` (
+  `order_id` int NOT NULL,
+  `items` int NOT NULL,
+  `order_date` date NOT NULL,
+  `username` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`order_id`),
+  KEY `username` (`username`),
+  CONSTRAINT `history_ibfk_1` FOREIGN KEY (`username`) REFERENCES `customer` (`username`)
 );
 
-
-CREATE TABLE fruit(
-	product_id INT,,
-	fruit_name VARCHAR(20),
-	fruit_price DECIMAL(4,2) NOT NULL,
-	fruit_quantity int NOT NULL,
-	fruit_picture VARCHAR(20) NOT NULL,
-	PRIMARY KEY (product_id),
-	FOREIGN KEY (product_id) references shopping_cart (product_id)
+CREATE TABLE `meat` (
+  `product_id` int NOT NULL,
+  `meat_name` varchar(20) DEFAULT NULL,
+  `meat_price` decimal(4,2) NOT NULL,
+  `meat_quantity` int NOT NULL,
+  `meat_picture` varchar(20) NOT NULL,
+  PRIMARY KEY (`product_id`)
 );
-CREATE TABLE vegetable(
-	product_id INT,
-	veg_name VARCHAR(20),
-	veg_price DECIMAL(4,2)NOT NULL,
-	veg_quantity INT NOT NULL,
-	veg_picture VARCHAR(20)NOT NULL,
-	PRIMARY KEY(product_id),
-	FOREIGN KEY(product_id) REFERENCES shopping_cart(product_id)
+CREATE TABLE `shopping_cart` (
+  `username` varchar(20) NOT NULL,
+  `product_id` int NOT NULL,
+  `product_price` decimal(5,2) NOT NULL,
+  `quantity` int NOT NULL,
+  PRIMARY KEY (`username`,`product_id`),
+  CONSTRAINT `shopping_cart_ibfk_1` FOREIGN KEY (`username`) REFERENCES `customer` (`username`)
 );
 
-CREATE TABLE meat(
-	product_id INT,
-	meat_name VARCHAR(20),
-	Meat_price DECIMAL(5,2) UNSIGNED NOT NULL,
-	meat_weight INT NOT NULL,
-	meat_picture VARCHAR(20)NOT NULL,
-	PRIMARY KEY(product_id),
-	FOREIGN KEY(product_id) REFERENCES shopping_cart(product_id)
-);
-
-
-
-
-
-
-
-
-CREATE TABLE user
-(
-  username VARCHAR(50) primary key,
-  `password` VARCHAR(50) NOT NULL,
-  email VARCHAR(50) NOT NULL
-);
-
-
-CREATE TABLE entity1 
-(
-  username VARCHAR(50) primary key,
-  `password` VARCHAR(50) NOT NULL,
-  email VARCHAR(50) NOT NULL
+CREATE TABLE `vegetable` (
+  `product_id` int NOT NULL,
+  `veg_name` varchar(20) DEFAULT NULL,
+  `veg_price` decimal(4,2) NOT NULL,
+  `veg_quantity` int NOT NULL,
+  `veg_picture` varchar(20) NOT NULL,
+  PRIMARY KEY (`product_id`)
 );
 
